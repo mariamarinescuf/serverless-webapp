94bd97d3b72e603fb944263541ba7d36cc54fbff
Maria Marinescu <maria@voxalyze.com>
2024-02-24 19:04:27 +0100

    Add tailwindCSS and auth social provider skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
 import React from "react";
 import { Amplify } from "aws-amplify";
-import type { WithAuthenticatorProps } from "@aws-amplify/ui-react";
-import { withAuthenticator } from "@aws-amplify/ui-react";
+import { Authenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
 import "./App.css";
 
 Amplify.configure({
   aws_project_region: "eu-central-1",
-  aws_cognito_identity_pool_id: process.env.REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID,
+  aws_cognito_identity_pool_id:
+    process.env.REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID,
   aws_cognito_region: "eu-central-1",
   aws_user_pools_id: process.env.REACT_APP_AWS_USER_POOLS_ID,
-  aws_user_pools_web_client_id: process.env.REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID,
-  oauth: {},
+  aws_user_pools_web_client_id:
+    process.env.REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID,
+  oauth: {
+    domain: "https://serverlesswebapp36f.auth.eu-central-1.amazoncognito.com",
+    scope: ["email"],
+    redirectSignIn: process.env.REACT_APP_AWS_REDIRECT_URL,
+    redirectSignOut: process.env.REACT_APP_AWS_REDIRECT_URL,
+    responseType: "code",
+    social_provider: {
+      google: {
+        app_id: process.env.REACT_APP_GMAIL_WEB_CLIENT_ID,
+        scope: "email",
+      },
+    },
+  },
   aws_cognito_username_attributes: ["EMAIL"],
   aws_cognito_social_providers: [],
   aws_cognito_signup_attributes: ["EMAIL"],
@@ -25,13 +38,15 @@ Amplify.configure({
   aws_cognito_verification_mechanisms: ["EMAIL"],
 } as any);
 
-export function App({ signOut, user }: WithAuthenticatorProps) {
+export default function App() {
   return (
-    <>
-      <h1>Hello Helloo {user?.username}</h1>
-      <button onClick={signOut}>Sign out</button>
-    </>
+    <Authenticator socialProviders={["amazon", "apple", "facebook", "google"]}>
+      {({ signOut, user }) => (
+        <main>
+          <h1>Hello Helloo {user?.signInDetails?.loginId}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </main>
+      )}
+    </Authenticator>
   );
 }
-
-export default withAuthenticator(App);
